a019cc49b347a444fafcdcac66cf8ff09d5ba3ef
jabran <jabran.noor@gmail.com>
2014-06-22 09:22:25 +0000

    top bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/features'
+
+  get 'welcome/faqs'
+
+  get 'welcome/pricing'
+
   resources :invoices
   
   root to: 'invoices#index'
